44470630a37b6075c07eefd0c3caad7ebff27e32
Duc Trong Pham <trong.phamtranduc@gmail.com>
2024-06-24 13:31:54 +0700

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using CrudApi.Models;
+namespace CrudApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
